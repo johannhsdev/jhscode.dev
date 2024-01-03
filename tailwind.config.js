ba9b0b3373e645b49e222ca1957bc250166e1608
Johann Hurtado <45208672+johannht2@users.noreply.github.com>
2024-01-02 21:48:20 -0400

    [MEJORA MIMES, Y CORRECCIONES]

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,18 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'home': 'url(../img/background.png)',
-        'home-mobile': 'url(../img/about-mobile.png)',
-        'home-table': 'url(../img/about-table.png)',
-        'about-screen': 'url(../img/background-about.png)',
-        'skills-screen': 'url(../img/background-skills.png)',
-        'courses-screen': 'url(../img/background-courses.png)',
-        'courses-page': 'url(../img/courses-page.png)',
-        'portfolio-screen': 'url(../img/background-portfolio.png)',
-        'portfolio-page': 'url(../img/portfolio-page.png)',
-        'blog-screen': 'url(../img/background-blogs.png)',
-        'blog-page': 'url(../img/blogs-page.png)',
-        'contact-screen': 'url(../img/background-contact.png)',
+        'home': 'url(../img/background.webp)',
+        'home-mobile': 'url(../img/about-mobile.webp)',
+        'home-table': 'url(../img/about-table.webp)',
+        'about-screen': 'url(../img/background-about.webp)',
+        'skills-screen': 'url(../img/background-skills.webp)',
+        'courses-screen': 'url(../img/background-courses.webp)',
+        'courses-page': 'url(../img/courses-page.webp)',
+        'portfolio-screen': 'url(../img/background-portfolio.webp)',
+        'portfolio-page': 'url(../img/portfolio-page.webp)',
+        'blog-screen': 'url(../img/background-blogs.webp)',
+        'blog-page': 'url(../img/blogs-page.webp)',
+        'contact-screen': 'url(../img/background-contact.webp)',
       },
 
       backgroundColor: theme =>({
